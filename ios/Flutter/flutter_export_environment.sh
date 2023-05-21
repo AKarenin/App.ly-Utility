3e12aa62aa43063b1407970e0bcec09f998c92ae
AKarenin <alexdhkil@gmail.com>
2023-05-21 11:10:44 +0900

    init commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/akarenin/env/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/akarenin/StudioProjects/schoolappfinal"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/akarenin/StudioProjects/schoolappfinal/.dart_tool/package_config.json"
